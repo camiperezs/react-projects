976ee304f79c00f55491601af2e2836d113567ce
Camila Perez <148892538+camiperezs@users.noreply.github.com>
2023-12-19 19:37:07 -0300

    agregar proyecto de contador de clics

---
+++ contador-de-clics/src/App.js
@@ -0,0 +1,41 @@
+import './App.css';
+import caritaLogo from './images/images.png';
+import Boton from './componentes/Boton';
+import Contador from './componentes/Contador';
+import { useState } from 'react';
+
+function App() {
+
+const [numClics, setNumClics ] = useState(0);
+
+const manejarClic = () => {
+    setNumClics(numClics +1);
+};
+
+const reiniciarContador = ()=> {
+    setNumClics(0);
+};
+
+  return (
+    <div className="App">
+        <div className='carita-logo-contenedor'>
+        <img className='carita-logo'
+            src={caritaLogo}
+            alt='Logo de carita feliz'/>
+        </div>
+        <div className='contenedor-principal'>
+            <Contador numClics={numClics} />
+            <Boton
+            texto= "Clic"
+            esBotonDeClic= {true}
+            manejarClic= {manejarClic}/>
+            <Boton
+              texto= "Reiniciar"
+              esBotonDeClic= {false}
+              manejarClic= {reiniciarContador}/>
+        </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ contador-de-clics/src/componentes/Contador.js
@@ -0,0 +1,9 @@
+import '../hojas-de-estilo/Contador.css';
+function Contador ({ numClics }){
+  return(
+  <div className='contador'>
+      {numClics}
+  </div>
+  );
+}
+export default Contador;
