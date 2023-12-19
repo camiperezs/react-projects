f4dac6face4e8c29af4f83f15eb3b9e4cfc844f8
Camila Perez <148892538+camiperezs@users.noreply.github.com>
2023-12-19 19:43:18 -0300

    agregar pyoecto calculadora

---
+++ calculadora/src/App.js
@@ -0,0 +1,70 @@
+import './App.css';
+import caritaLogo from './images/images.png';
+import Boton from './components/Boton';
+import Input from './components/Input';
+import BotonClear from './components/BotonClear';
+import { useState } from 'react';
+import { evaluate } from 'mathjs';
+
+function App() {
+
+  const [input, setInput] = useState('');
+
+  const agregarInput = val => {
+    setInput(input + val);
+  };
+
+  const calcularResultado = () => {
+    if(input){
+      setInput(evaluate(input));
+    }else{
+      alert('por favor ingrese valores para realizar los calculos')
+    }
+  };
+
+  return (
+    <div className='App'>
+      <div className='carita-logo-contenedor'> 
+      <img
+       src={caritaLogo}
+       className='carita-logo'
+       alt='Logo de calculadora' />
+      </div>
+
+      <div className='contenedor-calculadora'>
+        <Input input={input} />
+        <div className='fila'>
+          <Boton manejarClic={agregarInput}>1</Boton>
+          <Boton manejarClic={agregarInput}>2</Boton>
+          <Boton manejarClic={agregarInput}>3</Boton>
+          <Boton manejarClic={agregarInput}>+</Boton>
+        </div>
+        <div className='fila'>
+          <Boton manejarClic={agregarInput}>4</Boton>
+          <Boton manejarClic={agregarInput}>5</Boton>
+          <Boton manejarClic={agregarInput}>6</Boton>
+          <Boton manejarClic={agregarInput}>-</Boton>
+        </div>
+        <div className='fila'>
+          <Boton manejarClic={agregarInput}>7</Boton>
+          <Boton manejarClic={agregarInput}>8</Boton>
+          <Boton manejarClic={agregarInput}>9</Boton>
+          <Boton manejarClic={agregarInput}>*</Boton>
+        </div>
+        <div className='fila'>
+          <Boton manejarClic={calcularResultado}>=</Boton>
+          <Boton manejarClic={agregarInput}>0</Boton>
+          <Boton manejarClic={agregarInput}>.</Boton>
+          <Boton manejarClic={agregarInput}>/</Boton>
+        </div>
+        <div className='fila'>
+          <BotonClear manejarClear={() => setInput('')}>
+            Clear
+          </BotonClear>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ calculadora/src/components/Input.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import '../hojas-de-estilo/Input.css';
+const Input = ({ input }) => (
+    <div className='input'>
+        {input}
+    </div>
+);
+
+export default Input;
